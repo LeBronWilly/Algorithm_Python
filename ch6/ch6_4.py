a41a7a9c4463265689f29a3860585ac65c097763
Willy Fang <w70024@gmail.com>
2022-05-25 18:38:17 +0800

    Binary Tree 1

--- ch6/ch6_4.py
+++ ch6/ch6_4.py
@@ -1,24 +1,24 @@
 # ch6_4.py
-class Node():
+class Node():    # 節點類別
     def __init__(self, data=None):
         ''' 建立二元樹的節點 '''
-        self.data = data
-        self.left = None
-        self.right = None
+        self.data = data    # 父節點
+        self.left = None    # 左子節點
+        self.right = None   # 右子節點
 
     def insert(self, data):
         ''' 建立二元樹 '''
         if self.data:                           # 如果根節點存在
-            if data < self.data:                # 插入值小於目前節點值
-                if self.left:
+            if data < self.data:                # 如果插入值<目前節點值
+                if self.left:                   # 如果左子節點存在
                     self.left.insert(data)      # 遞迴呼叫往下一層
-                else:
-                    self.left = Node(data)      # 建立新節點存放資料
-            else:                               # 插入值大於目前節點值
-                if self.right:
-                    self.right.insert(data)
-                else:
-                    self.right = Node(data)                
+                else:                           # 如果左子節點不存在
+                    self.left = Node(data)      # 建立左子節點以存放資料
+            else:                               # 如果插入值>目前節點值
+                if self.right:                  # 如果右子節點存在
+                    self.right.insert(data)     # 遞迴呼叫往下一層
+                else:                           # 如果右子節點不存在
+                    self.right = Node(data)     # 建立右子節點以存放資料
         else:                                   # 如果根節點不存在
             self.data = data                    # 建立根節點
 
@@ -29,29 +29,9 @@ class Node():
         print(self.data)                        # 列印
         if self.right:                          # 如果右子節點存在
             self.right.inorder()                # 遞迴呼叫下一層
-        
+
 tree = Node()                                   # 建立二元樹物件
-datas = [10, 21, 5, 9, 13, 28]                  # 建立二元樹數據
+datas = [10, 21, 5, 9, 13, 28, 2, 30, 23]       # 建立二元樹數據
 for d in datas:
     tree.insert(d)                              # 分別插入數據
 tree.inorder()                                  # 中序列印
-
-
-
-
-    
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
