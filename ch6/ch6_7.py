514cc9418b025fde3d9cddde35dbe17b336468a4
Willy Fang <w70024@gmail.com>
2022-05-30 17:20:46 +0800

    Binary Tree 2

--- ch6/ch6_7.py
+++ ch6/ch6_7.py
@@ -1,44 +1,45 @@
 # ch6_7.py
-class Node():
+class Node():    # 節點類別
     def __init__(self, data=None):
         ''' 建立二元樹的節點 '''
-        self.data = data
-        self.left = None
-        self.right = None
+        self.data = data    # 父節點
+        self.left = None    # 左子節點
+        self.right = None   # 右子節點
 
     def insert(self, data):
         ''' 建立二元樹 '''
         if self.data:                           # 如果根節點存在
-            if data < self.data:                # 插入值小於目前節點值
-                if self.left:
+            if data < self.data:                # 如果插入值<目前節點值
+                if self.left:                   # 如果左子節點存在
                     self.left.insert(data)      # 遞迴呼叫往下一層
-                else:
-                    self.left = Node(data)      # 建立新節點存放資料
-            else:                               # 插入值大於目前節點值
-                if self.right:
-                    self.right.insert(data)
-                else:
-                    self.right = Node(data)                
+                else:                           # 如果左子節點不存在
+                    self.left = Node(data)      # 建立左子節點以存放資料
+            else:                               # 如果插入值>目前節點值
+                if self.right:                  # 如果右子節點存在
+                    self.right.insert(data)     # 遞迴呼叫往下一層
+                else:                           # 如果右子節點不存在
+                    self.right = Node(data)     # 建立右子節點以存放資料
         else:                                   # 如果根節點不存在
             self.data = data                    # 建立根節點
 
     def search(self, val):
         ''' 搜尋特定值 '''                      
-        if val < self.data:                     # 如果搜尋值小於目前節點值
+        if val < self.data:                     # 如果搜尋值<目前節點值
             if not self.left:                   # 如果左子節點不存在
-                return str(val) + " 不存在"
+                return str(val) + " 不存在"      # 此值不存在於樹中
             return self.left.search(val)        # 遞迴繼續往左子樹找尋
-        elif val > self.data:                   # 如果搜尋值大於目前節點值
+        elif val > self.data:                   # 如果搜尋值>目前節點值
             if not self.right:                  # 如果右子節點不存在
-                return str(val) + " 不存在"
-            return self.right.search(val)              
-        else:
-            return str(val) + " 找到了"
+                return str(val) + " 不存在"      # 此值不存在於樹中
+            return self.right.search(val)       # 遞迴繼續往右子樹找尋
+        else:                                   # 如果搜尋值=目前節點值
+            return str(val) + " 找到了"          # 此值已於樹中找到
 
 tree = Node()                                   # 建立二元樹物件
-datas = [10, 21, 5, 9, 13, 28]                  # 建立二元樹數據
+datas = [10, 21, 5, 9, 13, 28, 2, 30, 23]       # 建立二元樹數據
 for d in datas:
     tree.insert(d)                              # 分別插入數據
 
-n = eval(input("請輸入欲搜尋資料 : "))
-print(tree.search(n))
+# n = eval(input("請輸入欲搜尋資料 : "))
+for n in range(1, 14):
+    print(tree.search(n))
--- ch6/ch6_8.py
+++ ch6/ch6_8.py
@@ -1,24 +1,24 @@
 # ch6_8.py
-class Node():
+class Node():    # 節點類別
     def __init__(self, data=None):
         ''' 建立二元樹的節點 '''
-        self.data = data
-        self.left = None
-        self.right = None
+        self.data = data    # 父節點
+        self.left = None    # 左子節點
+        self.right = None   # 右子節點
 
     def insert(self, data):
         ''' 建立二元樹 '''
         if self.data:                           # 如果根節點存在
-            if data < self.data:                # 插入值小於目前節點值
-                if self.left:
+            if data < self.data:                # 如果插入值<目前節點值
+                if self.left:                   # 如果左子節點存在
                     self.left.insert(data)      # 遞迴呼叫往下一層
-                else:
-                    self.left = Node(data)      # 建立新節點存放資料
-            else:                               # 插入值大於目前節點值
-                if self.right:
-                    self.right.insert(data)
-                else:
-                    self.right = Node(data)                
+                else:                           # 如果左子節點不存在
+                    self.left = Node(data)      # 建立左子節點以存放資料
+            else:                               # 如果插入值>目前節點值
+                if self.right:                  # 如果右子節點存在
+                    self.right.insert(data)     # 遞迴呼叫往下一層
+                else:                           # 如果右子節點不存在
+                    self.right = Node(data)     # 建立右子節點以存放資料
         else:                                   # 如果根節點不存在
             self.data = data                    # 建立根節點
 
@@ -30,15 +30,16 @@ class Node():
         if self.right:                          # 如果右子節點存在
             self.right.inorder()                # 遞迴呼叫下一層
 
+
 class Delete_Node():
     def deleteNode(self, root, key):
         if root is None:                        # 二元樹不存在返回
             return None
-        if key < root.data:                     # 刪除值小於root值則往左
-            root.left = self.deleteNode(root.left, key)
+        if key < root.data:                     # 刪除值<root值則往左
+            root.left = self.deleteNode(root.left, key)         # 遞迴繼續往左子樹找尋
             return root
-        if key > root.data:                     # 刪除值大於root值則往右
-            root.right = self.deleteNode(root.right, key)
+        if key > root.data:                     # 刪除值>root值則往右
+            root.right = self.deleteNode(root.right, key)       # 遞迴繼續往右子樹找尋
             return root
         if root.left is None:                   # 左邊節點不存在
             new_root = root.right
@@ -66,12 +67,15 @@ class Delete_Node():
             node = node.right
         return node
 
+
 tree = Node()                                   # 建立二元樹物件
-datas = [10, 5, 21, 9, 13, 28, 3, 4, 1, 17, 32] # 建立二元樹數據
+# datas = [10, 5, 21, 9, 13, 28, 3, 4, 1, 17, 32] # 建立二元樹數據
+datas = [10, 5, 21, 9, 32]                      # 建立二元樹數據
 for d in datas:
     tree.insert(d)                              # 分別插入數據
 tree.inorder()                                  # 中序列印
-del_data = 5
+
+del_data = 21
 print("刪除 %d 資料後" % del_data)
 delete_obj = Delete_Node()                      # 建立刪除節點物件
 result = delete_obj.deleteNode(tree, del_data)  # 刪除操作
